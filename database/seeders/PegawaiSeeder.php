ec6bf8acf6de0b416266fbe35e9e09aa01d14479
Aldidwikusuma <aldidwik518@gmail.com>
2022-03-21 22:57:31 +0700

    edit data

--- database/seeders/PegawaiSeeder.php
+++ database/seeders/PegawaiSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PegawaiSeeder extends Seeder
 {
@@ -13,6 +14,31 @@ class PegawaiSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('pegawais')->insert([
+            [
+                "namaPegawai" => "Aldi Dwi Kusuma",
+                "jabatan" => "HRD",
+                "alamat" => "Gresik",
+                "noTelp" => "087704255655"
+            ],
+            [
+                "namaPegawai" => "Aria Pratama Effendi",
+                "jabatan" => "Manajer",
+                "alamat" => "Bangkalan",
+                "noTelp" => "081231527168"
+            ],
+            [
+                "namaPegawai" => "Wazir Qorni Abud",
+                "jabatan" => "Bendahara",
+                "alamat" => "Lamongan",
+                "noTelp" => "089530421926"
+            ],
+            [
+                "namaPegawai" => "Syahrul Eka Pratama",
+                "jabatan" => "Sekretaris",
+                "alamat" => "Surabaya",
+                "noTelp" => "085812616815"
+            ]
+        ]);
     }
 }
